6b6c1df5b2fc8dbef747f1f6b6c4d06ac43171f2
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-01-26 21:05:44 -0300

    Implement repositories and test

--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -8,6 +8,13 @@ namespace ACM.BL
 {
     public class CustomerRepository
     {
+        public CustomerRepository()
+        {
+            addressRepository = new AddressRepository();
+        }
+
+        private AddressRepository addressRepository { get; set; }
+
         /// <summary>
         /// Retrieve one customer.
         /// </summary>
@@ -26,6 +33,7 @@ namespace ACM.BL
                 customer.Email = "fbaggins@hobbiton.me";
                 customer.FirstName = "Frodo";
                 customer.LastName = "Baggins";
+                customer.Addresses = addressRepository.RetrieveByCustomerId(customerId).ToList();
             }
             return customer;
         }
--- Tests/ACM.BLTest/CustomerRepositoryTest.cs
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using ACM.BL;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ACM.BLTest
@@ -28,6 +29,59 @@ namespace ACM.BLTest
             Assert.Equal(expected.Email, actual.Email);
             Assert.Equal(expected.FirstName, actual.FirstName);
             Assert.Equal(expected.LastName, actual.LastName);
+        }
+
+        [Fact]
+        public void RetrieveExistingWithAddress()
+        {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                Email = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                Addresses = new List<Address>()
+                    {
+                        new Address()
+                        {
+                            AddressType = 1,
+                            Street = "Bag End",
+                            City = "Hobbiton",
+                            State = "Shire",
+                            Country = "Middle Earth",
+                            PostalCode = "144"
+                        },
+                        new Address()
+                        {
+                            AddressType = 2,
+                            Street = "Green Dragon",
+                            City = "Bywater",
+                            State = "Shire",
+                            Country = "Middle Earth",
+                            PostalCode = "146"
+                        }
+                    }
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Email, actual.Email);
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.Equal(expected.Addresses[i].AddressType, actual.Addresses[i].AddressType);
+                Assert.Equal(expected.Addresses[i].Street, actual.Addresses[i].Street);
+                Assert.Equal(expected.Addresses[i].City, actual.Addresses[i].City);
+                Assert.Equal(expected.Addresses[i].State, actual.Addresses[i].State);
+                Assert.Equal(expected.Addresses[i].Country, actual.Addresses[i].Country);
+                Assert.Equal(expected.Addresses[i].PostalCode, actual.Addresses[i].PostalCode);
+            }
         }
     }
 }
